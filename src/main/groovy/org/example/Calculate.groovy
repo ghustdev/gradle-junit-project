24cd6a1677e8f6638442ab5a2ab84989cf074c55
ghustdev <gustavocardoso0019@gmail.com>
2026-02-22 16:44:53 -0300

    Feat: learn TestFixture

--- src/main/groovy/org/example/Calculate.groovy
+++ src/main/groovy/org/example/Calculate.groovy
@@ -1,7 +1,14 @@
 package org.example
 
 class Calculate {
-    int calculateSum(int n1, int n2) {
-        return n1 + n2
+    int sum(int n1, int n2) {
+        n1 + n2
+    }
+
+    int divid(int n1, int n2) {
+        if (n2 == 0) {
+            throw new ArithmeticException()
+        }
+        n1 / n2
     }
 }
---
+++ src/test/groovy/org/example/CalculateTest.groovy
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Test
+import static org.junit.jupiter.api.Assertions.*
+
+class CalculateTest {
+	@Test
+	void sumTest() {
+		def calc = new Calculate()
+
+		assertEquals(30, calc.sum(10, 20), "2 + 2 deveria ser 4")
+	}
+	
+	// melhor que usar try catch
+//	@Test(expected = ArithmeticException.class)
+	@Test
+	void dividTest() {
+		try {
+			def calc = new Calculate()
+
+			int n1 = 10
+			int n2 = 1
+			calc.divid(n1, n2)
+		}
+		catch (ArithmeticException e) {
+			println e
+		}
+	}
+}
